50a5ba3fea4969e30234fe321382ce694bcf5520
iqra-ali <iqra.ali@student.moringaschool.com>
2023-05-20 18:13:12 +0300

    spicy dishes

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,23 +16,62 @@ spicy_foods = [
     },
 ]
 
+another_spicy_food = [
+    {
+        "name": "Pilau",
+        "cuisine": "Kenyan",
+        "heat_level": 5,
+    }
+]
+
+
 def get_names(spicy_foods):
+    names = []
+    for food in spicy_foods:
+        names.append(food["name"])
+    return names
     pass
 
 def get_spiciest_foods(spicy_foods):
+    spiciest_foods = []
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            spiciest_foods.append(food)
+    return spiciest_foods
     pass
 
 def print_spicy_foods(spicy_foods):
+    for food in spicy_foods:
+        heat_level = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
     pass
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+    return None
+
     pass
 
 def print_spiciest_foods(spicy_foods):
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            heat_level = "🌶" * food["heat_level"]
+            print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
     pass
 
 def get_average_heat_level(spicy_foods):
+    total_heat_level = 0
+    for food in spicy_foods:
+        total_heat_level += food["heat_level"]
+    if len(spicy_foods) > 0:
+        average_heat_level = total_heat_level / len(spicy_foods)
+    else:
+        average_heat_level = 0
+    return average_heat_level
     pass
 
 def create_spicy_food(spicy_foods, spicy_food):
+    spicy_foods.append(spicy_food)
     pass
